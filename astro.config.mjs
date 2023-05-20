8abe0f647d80867f7d0a86d0b984eb21b187fd61
Chakravarthy7102 <medicharlachakravarthi@gmail.com>
2023-05-20 23:05:31 +0530

    added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://Chakravarthy7102.github.io",
+  base:"/devchaks",
   integrations: [tailwind()]
 });
